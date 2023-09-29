ba33fe4ed5ea5e3babfaffd7f7a8f9a134478e29
carlos4565mana <carloscal61@gmail.com>
2023-09-29 12:22:09 -0300

    criado os metodos depositar e sacar na classe conta

---
+++ AgenciaBancaria/src/programa/Conta.java
@@ -0,0 +1,58 @@
+package programa;
+
+public class Conta {
+	private static int contadorContas = 1020;
+	
+	private int numeroConta;
+	private String agencia;
+	private Pessoa pessoa;
+	private Double saldo = 0.0;
+	
+	public Conta( Pessoa pessoa) {
+		this.numeroConta = contadorContas;
+		this.pessoa = pessoa;
+		contadorContas += 1;
+	
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void depositar(Double valor) {
+		if(valor > 0) {
+			this.saldo = getSaldo() + valor;
+			System.out.println("Seu depósito foi realizado com sucesso!");
+		}else {
+			System.out.println("Não foi possível realizar o depósito.");
+		}
+	}
+	
+	public void sacar(Double valor) {
+		if(valor > 0 && this.getSaldo()>= valor) {
+			this.saldo = getSaldo() - valor;
+			System.out.println("Saque realizado com sucesso!");
+		}else {
+			System.out.println("Não foi possivel realizar o Saque!");
+		}
+	}
+	
+	
+	
+}
--- AgenciaBancaria/src/programa/Pessoa.java
+++ AgenciaBancaria/src/programa/Pessoa.java
@@ -1,5 +1,61 @@
 package programa;
 
 public class Pessoa {
+	
+	private String nome;
+	private String email;
+	private String cpf;
+	private String senha;
+	
+	
+	public Pessoa(String nome, String email, String cpf, String senha) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.senha = senha;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
 
 }
